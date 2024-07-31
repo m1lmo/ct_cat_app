df459549598a04a6374bd1c5c0645da0e4ce1caf
m1lmo <omerfarukkoca35@gmail.com>
2024-07-31 19:57:33 +0300

    added homeAppBar strings to [ProjectStrings]

--- lib/core/constants/project_strings.dart
+++ lib/core/constants/project_strings.dart
@@ -1,5 +1,6 @@
 final class ProjectStrings {
   ProjectStrings._();
+  static const String homeAppBar = 'Cat App';
   static const String home = 'Home';
   static const String creditsToNextTier = 'Tier credits to next tier';
   static const String totalCredits = 'Total Credits';
